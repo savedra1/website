52d11255c88c8e7d9feb0d26676b54a2d0a49425
savedram <michael.savedra@ovo.com>
2024-03-19 17:02:39 +0000

    added s3 terraform infra.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,58 +1,100 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
+  backend "s3" {
+    bucket = var.STATE_BUCKET
+    key    = var.STATE_FILE
+    region = var.AWS_REGION
   }
 }
 
 provider "aws" {
-  region = "us-east-1" # Replace with your desired AWS region
+  region = var.AWS_REGION  # Replace with your desired AWS region
 }
 
-resource "aws_s3_bucket" "website_bucket" {
-  bucket = "msavedra-website"
-  acl    = "public-read"
-  website { # depricated
-    index_document = "index.html"
-    error_document = "error.html"
+
+# Create S3 bucket for hosting the static website
+resource "aws_s3_bucket" "static_website" {
+  bucket = var.SITE_BUCKET  # Replace with your desired bucket name
+}
+
+
+resource "aws_s3_bucket_public_access_block" "static_website_public_access_block" {
+  bucket = aws_s3_bucket.static_website.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+
+resource "aws_s3_bucket_website_configuration" "website_config" {
+  bucket = aws_s3_bucket.static_website.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
   }
 }
 
-# Configure Bucket Policy for Public Read Access
-resource "aws_s3_bucket_policy" "website_bucket_policy" {
-  bucket = aws_s3_bucket.website_bucket.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource = [
-          "${aws_s3_bucket.website_bucket.arn}/*"
-        ]
-      }
+data "aws_iam_policy_document" "allow_public_read" {
+  statement {
+    sid       = "PublicReadGetObject"
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = [
+      aws_s3_bucket.static_website.arn,  
+      "${aws_s3_bucket.static_website.arn}/*"
     ]
-  })
-}
 
-# Create a Hosted Zone in Route 53
-resource "aws_route53_zone" "hosted_zone" {
-  name = "msavedra.com" 
-}
-
-# Create an A Record and Alias to the S3 Website Endpoint
-resource "aws_route53_record" "website_record" {
-  zone_id = aws_route53_zone.hosted_zone.zone_id
-  name    = "msavedra.com" 
-  type    = "A"
-
-  alias {
-    name                   = aws_s3_bucket.website_bucket.website_endpoint
-    zone_id                = aws_s3_bucket.website_bucket.hosted_zone_id
-    evaluate_target_health = true
+    principals {
+      identifiers = [aws_s3_bucket.static_website.id]
+      type        = "*"
+    }
   }
 }
+
+resource "aws_s3_bucket_policy" "allow_public_read" {
+  bucket = aws_s3_bucket.static_website.id
+  policy = data.aws_iam_policy_document.allow_public_read.json
+}
+
+
+
+# ===== FILES
+
+resource "aws_s3_object" "index_page" {
+  bucket = aws_s3_bucket.static_website.id
+  key    = "index.html"
+  source = "../website/index.html"
+
+}
+
+resource "aws_s3_object" "error_page" {
+  bucket = aws_s3_bucket.static_website.id
+  key    = "error.html"
+  source = "../website/error.html"
+
+}
+
+resource "aws_s3_object" "css" {
+  bucket = aws_s3_bucket.static_website.id
+  key    = "styles.css"
+  source = "../website/styles.css"
+}
+
+resource "aws_s3_object" "terminal" {
+  bucket = aws_s3_bucket.static_website.id
+  key    = "terminal.js"
+  source = "../website/terminal.js"
+
+}
+
+resource "aws_s3_object" "backgroung" {
+  bucket = aws_s3_bucket.static_website.id
+  key    = "ZE9ZvL4.png"
+  source = "../website/ZE9ZvL4.png"
+
+}
+
