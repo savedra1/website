42bcd5d20cc4de0fb1d3c2777488757415982f14
savedram <michael.savedra@ovo.com>
2024-03-25 19:45:49 +0000

    updated tf backend to remove bad vars

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,25 @@
 terraform {
   backend "s3" {
-    bucket = vars.STATE_BUCKET
+    bucket = var.STATE_BUCKET
     key    = "dev/tfstate"
     region = "eu-west-1" #vars.AWS_REGION
   }
 }
 
 provider "aws" {
-  region     =  vars.AWS_REGION 
+  region     =  var.AWS_REGION 
 }
 
 module "route53" {
   source      = "./route53"
-  domain_name = vars.DOMAIN_NAME
-  region      = vars.AWS_REGION
+  domain_name = var.DOMAIN_NAME
+  region      = var.AWS_REGION
 
 }
 
 module "s3" {
   source           = "./s3"
-  site_bucket_name = vars.SITE_BUCKET
+  site_bucket_name = var.SITE_BUCKET
 }
 
 
