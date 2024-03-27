310e3608950793a6167a670dfce527bd796d1006
savedram <michael.savedra@ovo.com>
2024-03-27 16:31:17 +0000

    specified content type for index docs

--- terraform/s3/objects.tf
+++ terraform/s3/objects.tf
@@ -1,30 +1,32 @@
 
 resource "aws_s3_object" "index_page" {
-  bucket = aws_s3_bucket.static_website.id
-  key    = "index.html"
-  source = "../website/index.html"
-  etag = filemd5("../website/index.html")
+  bucket       = aws_s3_bucket.static_website.id
+  key          = "index.html"
+  source       = "../website/index.html"
+  etag         = filemd5("../website/index.html")
+  content_type = "text/html"
 }
 
 resource "aws_s3_object" "error_page" {
   bucket = aws_s3_bucket.static_website.id
   key    = "error.html"
   source = "../website/error.html"
-  etag = filemd5("../website/error.html")
+  etag   = filemd5("../website/error.html")
+  content_type = "text/html"
 }
 
 resource "aws_s3_object" "css" {
   bucket = aws_s3_bucket.static_website.id
   key    = "styles.css"
   source = "../website/styles.css"
-  etag = filemd5("../website/styles.css")
+  etag   = filemd5("../website/styles.css")
 }
 
 resource "aws_s3_object" "terminal" {
   bucket = aws_s3_bucket.static_website.id
   key    = "terminal.js"
   source = "../website/terminal.js"
-  etag = filemd5("../website/terminal.js")
+  etag   = filemd5("../website/terminal.js")
 }
 
 resource "aws_s3_object" "backgroung" {
