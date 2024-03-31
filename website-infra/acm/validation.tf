5fbe465bf20712147f557c8a1760a6fd98647109
savedram <michael.savedra@ovo.com>
2024-03-31 20:29:56 +0100

    added validation for tf cert

--- website-infra/acm/validation.tf
+++ website-infra/acm/validation.tf
@@ -3,5 +3,5 @@
 
 resource "aws_acm_certificate_validation" "cert_validation" {
   certificate_arn         = aws_acm_certificate.domain_cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.cert_record : record.fqdn]
+  validation_record_fqdns = [for record in var.cert_record : record.fqdn]
 }
