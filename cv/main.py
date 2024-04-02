2b7d5390f9b3ef5d2247c398829570182cd8498d
savedram <michael.savedra@ovo.com>
2024-04-02 18:38:27 +0100

    workflow test

--- cv/main.py
+++ cv/main.py
@@ -92,7 +92,7 @@ class Client:
         return json.loads(response.text)
     
     def export_as_pdf(self):
-        url = f"{self.base_url}/drive/v3/files/{self.doc_id}/export"
+        url = f"{self.base_url}/drive/v3/files/1KEXZPU3h4E-BetoxT_DSPvy4eOeOfxJvuMS8tdwhE-s"#{self.doc_id}/export"
         query = {
             "mimeType": "application/pdf"
         }
