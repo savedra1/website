e1be3d2c219ac3dd21e06fad7fb97a605382bd2b
savedram <michael.savedra@ovo.com>
2024-04-03 12:10:40 +0100

    fixed bug in pytest file

--- cv/tests/test_gcp_auth.py
+++ cv/tests/test_gcp_auth.py
@@ -16,15 +16,15 @@ def scopes():
 def sa_info():
     return {
         "type": "service_account",
-        "project_id": os.getenv("PROJECT_ID"),
-        "private_key_id": os.getenv("PRIVATE_KEY_ID"),
-        "private_key": os.getenv("SECRET_KEY"),
-        "client_email": os.getenv("CLIENT_EMAIL"),
-        "client_id": os.getenv("CLIENT_ID"),
+        "project_id": os.getenv("GCP_PROJECT_ID"),
+        "private_key_id": os.getenv("GCP_PRIVATE_KEY_ID"),
+        "private_key": os.getenv("GCP_SECRET_KEY"),
+        "client_email": os.getenv("GCP_CLIENT_EMAIL"),
+        "client_id": os.getenv("GCP_CLIENT_ID"),
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": os.getenv("CLIENT_CERT"),
+        "client_x509_cert_url": os.getenv("GCP_CLIENT_CERT"),
         "universe_domain": "googleapis.com"
     }
 
