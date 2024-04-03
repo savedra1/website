4adaf8630f398fcf47ffd4bd7a30b94b617e0f25
savedram <michael.savedra@ovo.com>
2024-04-03 12:53:00 +0100

    Added default cert to cv cloudfront dist.

--- cv-infra/cloudfront/distribution.tf
+++ cv-infra/cloudfront/distribution.tf
@@ -39,8 +39,9 @@ resource "aws_cloudfront_distribution" "cf_distribution" {
   }
 
   viewer_certificate {
-    ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2021"
+    cloudfront_default_certificate = true
+    ssl_support_method             = "sni-only"
+    minimum_protocol_version       = "TLSv1.2_2021"
   }
 
   http_version               = "http2"
