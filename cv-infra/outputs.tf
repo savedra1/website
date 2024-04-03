ebeb71bffe07b8c399e316ae9a295e4cd7274f2b
savedram <michael.savedra@ovo.com>
2024-04-03 12:20:25 +0100

    add temp pdf file for tf plan workflow

--- cv-infra/outputs.tf
+++ cv-infra/outputs.tf
@@ -2,6 +2,6 @@
 # Output cf domain to txt file for html file to read form
 resource "null_resource" "write_endpoint_to_file" { 
   provisioner "local-exec" {
-    command = "echo 'Cloudfront domain for cv: ${module.cloudfront.domain_name}'"
+    command = "echo 'Cloudfront domain for cv: ${module.cloudfront.cf_domain}'"
   }
 }
--- cv-infra/s3/objects.tf
+++ cv-infra/s3/objects.tf
@@ -2,7 +2,7 @@
 resource "aws_s3_object" "error_page" {
   bucket = aws_s3_bucket.cv_bucket.id
   key    = "cv.pdf"
-  source = "../cv.pdf"
-  etag   = filemd5("../cv.pdf")
+  source = "../../cv.pdf"
+  etag   = filemd5("../../cv.pdf")
   content_type = "application/pdf"
 }
